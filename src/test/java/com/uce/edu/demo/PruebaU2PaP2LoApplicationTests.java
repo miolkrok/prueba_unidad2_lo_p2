6f3f3c01481d137d91b79d0aff016e0ade4ca283
Luis Ortiz <luchoaam97@gmail.com>
2022-07-29 19:49:18 -0500

    COMMIT PRUEBA UNIDAD 2

---
+++ src/test/java/com/uce/edu/demo/PruebaU2PaP2LoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uce.edu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaU2PaP2LoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
